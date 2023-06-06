4e255832481eb105af10c8d30e6187bfe22c04e7
khan-1995 <khaleel1995@yahoo.com>
2023-06-06 23:53:09 +0530

    initial commit

---
+++ June2023/Hashing1.java
@@ -0,0 +1,14 @@
+/*Find the frequency/repition of numbers in a given array*/
+
+Class Hashing1 {
+
+
+	private static Map findFrequency(int[] numbers){
+	/*Code to added here*/
+	}
+
+	public static void main(String[] args){
+	 
+	}
+
+}

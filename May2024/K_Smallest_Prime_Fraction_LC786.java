b74e0786a4e0f826b2451d272d666670c4a28de4
khan khaleelullah khan <41608140+khan-1995@users.noreply.github.com>
2024-05-11 19:47:08 +0530

    added optimized approach

--- May2024/K_Smallest_Prime_Fraction_LC786.java
+++ May2024/K_Smallest_Prime_Fraction_LC786.java
@@ -1,4 +1,26 @@
 class Solution {
+     public int[] getCount(int[] arr,double target){
+
+        int count = 0;
+        int n = arr.length;
+        int numer = arr[0];
+        int denom = arr[n-1];
+        int i=0;
+        for(int j=1;j<arr.length;j++){
+            
+            while((arr[i]/(double) arr[j]) <=target){
+                i++;
+            }
+            count+=i;
+
+            if(i>0 && (arr[i-1]/(double)arr[j])>(numer/(double) denom)){
+                numer = arr[i-1];
+                denom = arr[j];
+            }
+        }
+
+        return new int[]{count,numer,denom};
+    }
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
         // Create a priority queue to store pairs of fractions
         PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
@@ -32,5 +54,24 @@ class Solution {
         // the top of the priority queue
         double[] result = pq.poll();
         return new int[]{arr[(int) result[1]], arr[(int) result[2]]};
+        /*Optimized O(logn)
+           double low = 0.0;
+           double high = 1.0;
+
+            while(low<high){
+                double mid = (double)((low+high)/2);
+    
+                int[] count = getCount(arr,mid);
+                if(count[0]==k){
+                    return new int[]{count[1],count[2]};
+                }else if(count[0]>k){
+                    high = mid;
+                }else{
+                    low = mid;
+                }
+            }
+
+            return null;
+        */
     }
 }

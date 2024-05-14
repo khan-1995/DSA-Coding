ce530d3ac972b60359c24d53f86b63ae3d3d066e
khan khaleelullah khan <41608140+khan-1995@users.noreply.github.com>
2024-05-14 16:54:50 +0530

    Update Score_After_Flipping_Matrix_LC861.java

--- May2024/Score_After_Flipping_Matrix_LC861.java
+++ May2024/Score_After_Flipping_Matrix_LC861.java
@@ -3,7 +3,9 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        // Set score to summation of first column
+        /*Without manipulating i/p*/
+        
+        // Set score to summation of first colum
         int score = (1 << (n - 1)) * m;
 
         // Loop over all other columns
@@ -24,5 +26,48 @@ class Solution {
         }
 
         return score;
+
+        /*With manipulating the i/p 
+        
+         - Look at left most bit 
+         - There are only two rules that you need to know in this problem. If the first number in the row is 0, flip the row. If the count of 0 in the col is greater than the count of 1, flip the col.
+
+         
+            int row = grid.length;
+            int col = grid[0].length;
+    
+            for(int r=0;r<row;r++){
+                if(grid[r][0]==0){
+                    for(int c=0;c<col;c++){
+                        grid[r][c] =  grid[r][c]==0 ? 1 : 0;
+                    }
+                }
+            }
+    
+             
+            for(int c=0;c<col;c++){
+                int one_count = 0;
+                for(int r=0;r<row;r++){
+                    one_count+=grid[r][c];
+                }
+    
+                if(one_count < (row-one_count)){
+                 for(int r=0;r<row;r++){
+                    grid[r][c] =  grid[r][c]==0 ? 1 : 0;
+                  }
+                }
+            }
+    
+            int res = 0;
+            for(int r=0;r<row;r++){
+                for(int c=0;c<col;c++){
+                    // System.out.print(grid[r][c]+", ");
+                    res+= grid[r][c] << (col-c-1);
+                }
+                // System.out.println("");
+            }
+            
+            return res; 
+        */
     }
 }

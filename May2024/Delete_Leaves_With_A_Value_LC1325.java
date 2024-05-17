d09459a3bd5fa0dc3c9405775e740c36c833f6a8
khan khaleelullah khan <41608140+khan-1995@users.noreply.github.com>
2024-05-17 23:27:26 +0530

    Tree DFS / BFS

---
+++ May2024/Delete_Leaves_With_A_Value_LC1325.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public TreeNode traverse(TreeNode root,int target){
+
+        if(root==null){
+            return null;
+        }
+
+        root.left = traverse(root.left,target);
+        root.right = traverse(root.right,target);
+
+        if(root.left==null && root.right==null && root.val==target){
+            return null;
+        }
+
+        return root;
+    }
+
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+
+
+        return traverse(root,target);
+    }
+}
